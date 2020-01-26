0b1d3c776269d8c54e431e1e173fe56518f8172c
gerulisss <rokodarbinis@gmail.com>
2020-01-26 14:37:20 +0200

    product table add column type

--- resources/views/product/index.blade.php
+++ resources/views/product/index.blade.php
@@ -1,54 +1,6 @@
 @extends('layouts.app')
 @section('messages')
 @section('content')
-{{-- <div class="container">
-  <h2>Produktų sąrašas</h2>
-  <a class="btn btn-outline-primary btn-sm" href="{{route('product.create')}}">Sukurti nauja</a>
-  <br>
-  <br>
-  <table class="table table-bordered" id="laravel">
-     <thead>
-        <tr>
-           <th>ID</th>
-           <th>Dydis</th>
-           <th>Aprašymas</th>
-           <th>Kaina</th>
-           <th>Nuolaida</th>
-           <th>Nuotrauka</th>
-           <th>Prioritetas</th>
-           <th>Grupė</th>
-           <th>Sukurtas</th>
-           <th>Atnaujintas</th>
-           <th></th>
-        </tr>
-     </thead>
-     <tbody>
-        @foreach($products as $product)
-        <tr>
-          <td>{{$product->id}}</td>
-          <td>{{ $product->size_title }}</td>
-          <td>{!! \Illuminate\Support\Str::words($product->desc, 3,'....')  !!}</td>
-          <td>{{ $product->price}}</td>
-          <td>{{ $product->discount}}</td>
-          <td>{{$product->photo}}</td>
-          <td>{{$product->priority}}</td>
-          <td>{{$product->group->id}}</td>
-          <td>{{$product->created_at}}</td>
-          <td>{{$product->updated_at}}</td>
-          <td>
-          <form method="POST" action="{{route('product.destroy', [$product])}}">
-            <a style="text-decoration:none;" href="{{route('product.edit',[$product])}}"><button type="button" class="btn btn-outline-primary btn-sm">Redaguoti</button></a>
-            @csrf
-          <button type="submit" class="btn btn-outline-danger btn-sm">Ištrinti</button>
-        </form>
-      </td>
-        </tr>
-        @endforeach
-        {{ $products->links() }}
-     </tbody>
-  </table>
-</div> --}}
-
 <div class="container">
   <div class="row justify-content-center">
       <div class="col-md-12">
@@ -68,9 +20,10 @@
                         <th>Nuotrauka</th>
                         <th>Prioritetas</th>
                         <th>Grupė</th>
+                        <th>Tipas</th>
                         <th>Sukurtas</th>
                         <th>Atnaujintas</th>
-                        <th width="240px">Action</th>
+                        <th width="240px"></th>
                       </tr>
                     </thead>
                     <tbody>
@@ -84,6 +37,7 @@
                         <td>{{$product->photo}}</td>
                         <td>{{$product->priority}}</td>
                         <td>{{$product->group->id}}</td>
+                        <td>{{$product->group->type->id}}</td>
                         <td>{{$product->created_at}}</td>
                         <td>{{$product->updated_at}}</td>
                         <td>
@@ -103,5 +57,4 @@
           </div>
       </div>
   </div>
- 
-@endsection
+ @endsection
