d24a30f7e4067bc0a584886fead6a1394b03124f
gerulisss <rokodarbinis@gmail.com>
2020-01-29 18:07:14 +0200

    update code

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -13,4 +13,5 @@
         <link href="{{ asset('css/main.css') }}" rel="stylesheet">
     </head>
     <script src="{{ asset('/js/main.js') }}" defer></script>
+    <script src="{{ asset('/js/app.js') }}" defer></script>
 </html>
